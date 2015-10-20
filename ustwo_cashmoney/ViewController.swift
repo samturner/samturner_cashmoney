8243421a33a75459fe788e386aaff5d735d9c85a
Sam Turner <samturner.23@me.com>
2015-10-20 15:37:31 +1100

    Added UI elements

--- ustwo_cashmoney/ViewController.swift
+++ ustwo_cashmoney/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var amountInputField: UITextField!
+    @IBOutlet var amountOutputField: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        amountInputField.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
